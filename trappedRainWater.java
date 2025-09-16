c116bd32d88f70be376e64040d4725c0f83a084b
notsuperhero <pramilaunni3@gmail.com>
2025-09-16 18:46:37 +0530

    buyAndSellStock

--- trappedRainWater.java
+++ trappedRainWater.java
@@ -1,4 +1,34 @@
 public class trappedRainWater {
+
+    public static int TrappedRainWater(int[]height){
+        int n = height.length;
+        int leftMax [] = new int[height.length];
+        leftMax[0] = height[0];
+        for(int i=1;i<n;i++){
+            leftMax[i] = Math.max(height[i],leftMax[i-1]);
+        }
+        int rightMax []= new int[height.length];
+        rightMax[n-1] = height[n-1]; 
+        for(int i=n-2;i>0;i--){
+            rightMax[i] = Math.max(height[i],rightMax[i-1]);
+        }
+        int trappedWater = 0;
+        for(int i=0;i<n;i++){
+            int waterlevel = Math.min(leftMax[i],rightMax[i]);
+            trappedWater = waterlevel - height[i];
+        }
+        return trappedWater;
+    }
+    public static void main(String[]args){
+        int height [] = {4,2,0,6,3,2,5};
+    }
+}
+
+
+
+
+
+
     public static int TrappedRainWater(int[] height){
         int n = height.length;
         int leftMax [] = new int[height.length];
@@ -24,4 +54,3 @@ public class trappedRainWater {
         int result = TrappedRainWater(height);   // call the method
         System.out.println(result);   
     }
-}
